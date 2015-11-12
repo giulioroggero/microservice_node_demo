32b0d8eff488f4002a107266c4a8a1ab617f43cf
Giulio <giulio.roggero@makeitapp.eu>
2015-11-12 12:39:58 +0100

    catalogue with real lookps

--- services/report_catalogue@0.0.1/test/test.js
+++ services/report_catalogue@0.0.1/test/test.js
@@ -10,22 +10,124 @@ var hw = require('../');
 /*jshint camelcase: false */
 var dummy = {
     dropdown_list: [{
-        name: 'dropdown1',
+        name: 'Tipo Report',
         values: [{
-            key: 'chiave1',
-            value: 'valore1'
+            key: 'produzione',
+            value: 'Produzione'
         }, {
             key: 'chiave2',
             value: 'valore3'
         }]
     }, {
-        name: 'dropdown2',
+        name: 'Silos',
         values: [{
-            key: 'chiave1',
-            value: 'valore1'
+            key: 'ubb_access',
+            value: 'UBB Access'
         }, {
-            key: 'chiave2',
-            value: 'valore3'
+            key: 'all_ip',
+            value: 'All Ip & Backbone'
+        }, {
+            key: 'core',
+            value: 'Core, Platforms & Virtual'
+        }]
+    }, {
+        name: 'Polo',
+        values: [{
+            key: 'roma',
+            value: 'Roma'
+        }, {
+            key: 'milano',
+            value: 'Milano'
+        }]
+    }, {
+        name: 'Isola',
+        values: [{
+            key: 'access_network',
+            value: 'Access Network (per UBB Access) '
+        }, {
+            key: 'backbone_ip',
+            value: 'Backbone IP (per All Ip & Backbone)'
+        }, {
+            key: 'backbone_tx',
+            value: 'Backbone TX (per All Ip & Backbone)'
+        }, {
+            key: 'mobile',
+            value: 'Mobile (per Core, Platforms & Virtual Functions)'
+        }, {
+            key: 'fisso',
+            value: 'Fisso (per Core, Platforms & Virtual Functions) '
+        }]
+    },{
+        name: 'Famiglia',
+        values: [{
+            key: 'adsl',
+            value: 'ADSL'
+        }, {
+            key: 'phd',
+            value: 'PHD'
+        }, {
+            key: 'sdh',
+            value: 'SDH'
+        }, {
+            key: 'ptn',
+            value: 'PTN'
+        }]
+    },{
+        name: 'Vendor',
+        values: [{
+            key: 'alcatel',
+            value: 'alcatel'
+        }, {
+            key: 'ericsson',
+            value: 'Ericsson'
+        }, {
+            key: 'esm',
+            value: 'ESM'
+        }, {
+            key: 'huawei',
+            value: 'Huawei'
+        }, {
+            key: 'italtel',
+            value: 'Italtel'
+        }, {
+            key: 'nokia',
+            value: 'Nokia'
+        }, {
+            key: 'siemens',
+            value: 'Siemens'
+        }, {
+            key: 'teko',
+            value: 'TEKO'
+        }, {
+            key: 'altro',
+            value: 'Altro'
+        }]
+    },{
+        name: 'Piattaforma',
+        values: [{
+            key: 'em',
+            value: 'EM'
+        }, {
+            key: 'gsm',
+            value: 'GSM'
+        }, {
+            key: 'umts',
+            value: 'UMTS'
+        }, {
+            key: 'lte',
+            value: 'LTE'
+        }, {
+            key: 'teko',
+            value: 'TEKO'
+        }, {
+            key: 'wll',
+            value: 'WLL'
+        }, {
+            key: 'gprs',
+            value: 'GPRS'
+        }, {
+            key: 'altro',
+            value: 'Altro'
         }]
     }]
 };
